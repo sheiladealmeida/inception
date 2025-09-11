ef43df52cc38687afd21464ed58e044777de16d2
Sheila <sheila.dealmeida22@gmail.com>
2025-09-11 16:07:41 +0100

    version fix

--- srcs/requirements/wordpress/wp-install.sh
+++ srcs/requirements/wordpress/wp-install.sh
@@ -8,4 +8,4 @@ sleep 5
 ./wp-cli.phar core install --url=${WP_DOMAIN} --title=${WP_TITLE} --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL} --allow-root
 ./wp-cli.phar user create ${WP_GUEST_USER} ${WP_GUEST_EMAIL} --role=subscriber --user_pass=${WP_GUEST_PASSWORD} --allow-root
 
-php-fpm8.4 -F
+php-fpm8.2 -F
