6427c9f72081dfc88a8dcd136ef4c2c8a9c78e9f
Sheiladealmeida <sheila.dealmeida22@gmail.com>
2025-09-14 20:37:30 +0100

    fix db pass

--- Makefile
+++ Makefile
@@ -1,11 +1,20 @@
 all: build
 data:
 	@cp ~/.env ./srcs/
-	mkdir -p /home/shrodrig/data/mariadb && mkdir -p /home/shrodrig/data/wordpress
+	mkdir -p /home/sheila/data/mariadb && mkdir -p /home/sheila/data/wordpress
 
 build: data
-	cd srcs && docker compose up --build
+	cd srcs && docker compose up -d --build
 
-clean:
+down:
+	cd srcs && docker compose down
+
+clean: 
+	cd srcs && docker compose down -v --rmi all
+	
+fclean: clean
+	sudo rm -rf /home/sheila/data/mariadb /home/sheila/data/wordpress
 	cd srcs && docker system prune -a
-	docker volume rm $$(docker volume ls -q)
+	@if [ "$$(docker volume ls -q)" ]; then \
+    	docker volume rm $$(docker volume ls -q); \
+	fi
--- srcs/requirements/mariadb/mariadb-run.sh
+++ srcs/requirements/mariadb/mariadb-run.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-echo "CREATE DATABASE IF NOT EXISTS $DB_NAME;" > /etc/mysql/init.sql
-echo "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';" >> /etc/mysql/init.sql
-echo "GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD' WITH GRANT OPTION;" >> /etc/mysql/init.sql
+echo "CREATE DATABASE IF NOT EXISTS ${DB_NAME};" >> /etc/mysql/init.sql
+echo "CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';" >> /etc/mysql/init.sql
+echo "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';" >> /etc/mysql/init.sql
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';" >> /etc/mysql/init.sql
 echo "FLUSH PRIVILEGES;" >> /etc/mysql/init.sql
+
 sleep 5
 mysql_install_db 
 mysqld
